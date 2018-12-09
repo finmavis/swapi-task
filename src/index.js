a3d79b8fc7ed2ddf8ba606fd3d47d995d06d2e67
Fin Mavis <fin.mavis@yahoo.com>
2018-12-09 16:02:55 +0700

    Edit: Change back Router prop basename to empty

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const app = (
-  <Router basename='/swapi-task'>
+  <Router>
     <App />
   </Router>
 );
