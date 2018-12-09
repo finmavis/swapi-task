f02ac711d7ac56b920d8652bf08ea866fb70b28f
Fin Mavis <fin.mavis@yahoo.com>
2018-12-09 16:00:19 +0700

    Edit: Basename on Router prop

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const app = (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router basename='/swapi-task'>
     <App />
   </Router>
 );
