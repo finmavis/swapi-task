a1dbc8512fce32b6c1ac2b422291c8463eada461
Aris Rinardi <aris.rinardi@amarbank.co.id>
2019-08-19 17:34:39 +0700

    Show error if format date get called with wrong format

--- src/shared/helpers/format-date.js
+++ src/shared/helpers/format-date.js
@@ -2,5 +2,18 @@ import { MONTH_NAMES } from '../constants/month-name.constant';
 
 export function formatDate(date) {
   const [year, month, day] = date.split('-');
+
+  if (
+    !year ||
+    !month ||
+    !day ||
+    year.length < 4 ||
+    parseInt(month) < 1 ||
+    parseInt(month) > 12
+  ) {
+    throw new Error(
+      'formatDate arguments must be called with YYYY-MM-DD format',
+    );
+  }
   return `${MONTH_NAMES[month - 1]} ${day}, ${year}`;
 }
