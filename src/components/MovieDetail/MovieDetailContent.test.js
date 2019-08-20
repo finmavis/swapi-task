2abbfceae0e25c3e04355755986f7d2d1024324e
Aris Rinardi <aris.rinardi@amarbank.co.id>
2019-08-20 15:34:42 +0700

    Extract mock data to constant

--- src/components/MovieDetail/MovieDetailContent.test.js
+++ src/components/MovieDetail/MovieDetailContent.test.js
@@ -4,38 +4,26 @@ import { render } from '@testing-library/react';
 
 import MovieDetailContent from './MovieDetailContent';
 
+import { MOVIE_DETAIL_MOCK_DATA } from '../../shared/constants/mock-data.constant';
+
 describe('<MovieDetailContent />', () => {
   it('Should render properly', () => {
-    const data = {
-      title: 'Movie Title',
-      director: 'John Doe',
-      producer: 'Jonny Stark',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque animi aliquam porro odio excepturi ad odit cum repellat reiciendis enim! Saepe quasi vero, quae accusantium quidem quibusdam excepturi omnis labore!',
-      characters: ['Capt', 'Jonny', 'Sins', 'Stark', 'Yoda'],
-      planets: ['Earth', 'EA-45'],
-      species: [],
-      starships: ['Skater', 'Car', 'Motorcycle'],
-      vehicles: [],
-    };
     const { getByText } = render(
       <Router>
-        <MovieDetailContent {...data} />
+        <MovieDetailContent {...MOVIE_DETAIL_MOCK_DATA} />
       </Router>,
     );
 
-    expect(getByText(/movie title/i)).toBeInTheDocument();
-    expect(getByText(/john doe/i)).toBeInTheDocument();
-    expect(getByText(/jonny stark/i)).toBeInTheDocument();
+    expect(getByText(/something title/i)).toBeInTheDocument();
+    expect(getByText(/Jonny Sins/i)).toBeInTheDocument();
+    expect(getByText(/Jonny Stark/i)).toBeInTheDocument();
     expect(getByText(/lorem/i)).toBeInTheDocument();
     expect(getByText(/yoda/i)).toBeInTheDocument();
     expect(getByText(/ea-45/i)).toBeInTheDocument();
+    expect(getByText(/cat/i)).toBeInTheDocument();
     expect(
-      getByText(/we don't have any species data yet!/i),
+      getByText(/we don't have any starships data yet!/i),
     ).toBeInTheDocument();
-    expect(getByText(/skater/i)).toBeInTheDocument();
-    expect(
-      getByText(/we don't have any vehicles data yet!/i),
-    ).toBeInTheDocument();
+    expect(getByText(/car/i)).toBeInTheDocument();
   });
 });
---
+++ src/shared/constants/mock-data.constant.js
@@ -0,0 +1,12 @@
+export const MOVIE_DETAIL_MOCK_DATA = {
+  title: 'Something Title',
+  director: 'Jonny Sins',
+  producer: 'Jonny Stark',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores fugit nostrum quaerat voluptatem dicta id dolorum modi dignissimos eum, magni alias, voluptate culpa tempora unde quos architecto assumenda eligendi.',
+  characters: ['Nikkar', 'Saitama', 'Genos', 'Yoda'],
+  planets: ['Earth', 'EA-45', 'Youranus'],
+  species: ['Cat', 'Dog'],
+  starships: [],
+  vehicles: ['Car', 'Fly'],
+};
