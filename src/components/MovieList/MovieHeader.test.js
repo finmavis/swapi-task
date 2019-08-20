092b89f17008c8a107daed0fc3e9198293498526
Aris Rinardi <aris.rinardi@amarbank.co.id>
2019-08-20 13:15:32 +0700

    Update mock on Movie Header test

--- src/components/MovieList/MovieHeader.test.js
+++ src/components/MovieList/MovieHeader.test.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
-import * as helpers from '../../shared/helpers/format-date';
+import { formatDate } from '../../shared/helpers/format-date';
 
 import MovieHeader from './MovieHeader';
 
+jest.mock('../../shared/helpers/format-date');
+
+afterEach(() => {
+  formatDate.mockReset();
+});
+
 describe('<MovieHeader />', () => {
   it('Should render properly', () => {
-    // Mocks
-    jest.spyOn(helpers, 'formatDate');
-    helpers.formatDate.mockImplementation(date => date);
-
     // Render
     const { getByText } = render(
       <Router>
@@ -22,10 +24,7 @@ describe('<MovieHeader />', () => {
     // Assert
     expect(getByText(/movie title/i)).toBeInTheDocument();
     expect(getByText(/movie title/i)).toHaveAttribute('href', '/movie/1');
-    expect(helpers.formatDate).toHaveBeenCalledTimes(1);
-    expect(helpers.formatDate).toHaveBeenCalledWith('2010-10-20');
-
-    // Cleanup
-    helpers.formatDate.mockRestore();
+    expect(formatDate).toHaveBeenCalledTimes(1);
+    expect(formatDate).toHaveBeenCalledWith('2010-10-20');
   });
 });
