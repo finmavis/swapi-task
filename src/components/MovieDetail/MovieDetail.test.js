10805cfae74b9866ac975b790d1dd4ef27b29b5f
Aris Rinardi <aris.rinardi@amarbank.co.id>
2019-08-20 15:45:16 +0700

    Add Movie Detail test

---
+++ src/components/MovieDetail/MovieDetail.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import MovieDetail from './MovieDetail';
+import useMovieDetail from '../../shared/hooks/useMovieDetail';
+
+import { MOVIE_DETAIL_MOCK_DATA } from '../../shared/constants/mock-data.constant';
+
+jest.mock('../../shared/hooks/useMovieDetail');
+
+afterEach(() => {
+  useMovieDetail.mockReset();
+});
+
+describe('<MovieDetail />', () => {
+  it('Should render Loader component', () => {
+    const mockMatch = {
+      params: {
+        id: '1',
+      },
+    };
+    useMovieDetail.mockImplementation(() => {
+      return {
+        loading: true,
+      };
+    });
+    const { container } = render(
+      <Router>
+        <MovieDetail match={mockMatch} />
+      </Router>,
+    );
+
+    expect(container.querySelector('svg')).toBeInTheDocument();
+  });
+
+  it('Should render Movie Detail properly', () => {
+    const mockMatch = {
+      params: {
+        id: '1',
+      },
+    };
+    useMovieDetail.mockImplementation(() => {
+      return {
+        loading: false,
+        movieDetail: {
+          ...MOVIE_DETAIL_MOCK_DATA,
+        },
+      };
+    });
+    const { getByText } = render(
+      <Router>
+        <MovieDetail match={mockMatch} />
+      </Router>,
+    );
+
+    expect(getByText(/something title/i)).toBeInTheDocument();
+    expect(getByText(/Jonny Sins/i)).toBeInTheDocument();
+    expect(getByText(/Jonny Stark/i)).toBeInTheDocument();
+    expect(getByText(/lorem/i)).toBeInTheDocument();
+    expect(getByText(/yoda/i)).toBeInTheDocument();
+    expect(getByText(/ea-45/i)).toBeInTheDocument();
+    expect(getByText(/cat/i)).toBeInTheDocument();
+    expect(
+      getByText(/we don't have any starships data yet!/i),
+    ).toBeInTheDocument();
+    expect(getByText(/car/i)).toBeInTheDocument();
+  });
+});
